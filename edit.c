4ea65e6edc65595a963d9b9e87ed70ea79d79aff
tg <tg@mirbsd.org>
2003-08-31 20:54:03 +0000

    Merge import of OpenBSD source, ports and XF4 tree.
    While here,
    o clean up differences where possible
    o whitespace cleanup
    o ifdef ./. if defined()
    o '...' ./. "..."
    o echo foo > bar ./. echo foo >bar
    o `...` ./. $(...) ./. $$(...)
    o `...' ./. '...'
    o modernize "our" tree, e.g. WWW in ports
    o fix some typos and brainos introduced when renaming OpenBSD to MirBSD
    o use hardware 80387 by default
    o migrate Apache 1.3.28 OpenBSD ./. MirBSD ./. KAME
    o work around as many CVS bugs as possible (add back/delete files, ...)
    
    Synchronize stuff, ready for ongoing changes.

--- edit.c
+++ edit.c
@@ -1,5 +1,5 @@
-/*	$MirBSD: edit.c,v 1.3 2003/06/29 19:45:40 tg Exp $	*/
-/*	$OpenBSD: edit.c,v 1.17 2003/06/26 00:09:45 deraadt Exp $	*/
+/*	$MirBSD: edit.c,v 1.4 2003/08/31 20:54:02 tg Exp $	*/
+/*	$OpenBSD: edit.c,v 1.18 2003/08/22 18:17:10 fgsch Exp $	*/
 
 /*
  * Command line editing - common code
@@ -773,7 +773,7 @@ x_locate_word(buf, buflen, pos, startp, is_commandp)
 		;
 	/* Go forwards to end of word */
 	for (end = start; end < buflen && IS_WORDC(buf[end]); end++) {
-		if (buf[end] == '\\' && (end+1) < buflen && buf[end+1] == ' ')
+		if (buf[end] == '\\' && (end+1) < buflen)
 			end++;
 	}
 
