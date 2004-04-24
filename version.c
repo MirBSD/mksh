461c2a76e37a69c0acaab1076facf843d25d728e
tg <tg@mirbsd.org>
2004-04-24 21:45:37 +0000

    bump to mirbsdksh 1.5

--- version.c
+++ version.c
@@ -1,4 +1,4 @@
-/* $MirBSD: version.c,v 1.4 2004/04/17 00:50:28 tg Exp $ */
+/* $MirBSD: version.c,v 1.5 2004/04/24 21:45:36 tg Exp $ */
 /* $OpenBSD: version.c,v 1.12 1999/07/14 13:37:24 millert Exp $	*/
 
 /*
@@ -8,4 +8,4 @@
 #include "sh.h"
 
 const char ksh_version[] =
-	"@(#)PD KSH v5.2.14 99/07/13.2 MirOS $Revision: 1.4 $";
+	"@(#)PD KSH v5.2.14 99/07/13.2 MirOS $Revision: 1.5 $";
