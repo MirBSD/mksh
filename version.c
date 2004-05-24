cc834563b3ef8e45f8aee9bbcd6aab0833b2439b
tg <tg@mirbsd.org>
2004-05-24 20:50:10 +0000

    bump to mirbsdksh-1_10 tested on * MirOS (native Makefile) * MirOS * FreeBSD (some perl warnings) * Interix/SFU 3.5 (perl aborts) * Mac OSX * GNU/Linux 2.6 * Solaris

--- version.c
+++ version.c
@@ -1,4 +1,4 @@
-/* $MirBSD: version.c,v 1.6 2004/04/27 20:00:22 tg Exp $ */
+/* $MirBSD: version.c,v 1.10 2004/05/24 20:50:08 tg Stab $ */
 /* $OpenBSD: version.c,v 1.12 1999/07/14 13:37:24 millert Exp $	*/
 
 /*
@@ -8,4 +8,4 @@
 #include "sh.h"
 
 const char ksh_version[] =
-	"@(#)PD KSH v5.2.14 99/07/13.2 MirOS $Revision: 1.6 $";
+	"@(#)PD KSH v5.2.14 99/07/13.2 MirOS $Revision: 1.10 $";
