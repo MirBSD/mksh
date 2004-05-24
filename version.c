259438b733b589bc29fc4578f032f3d616c7c761
root <root@mirbsd.org>
2004-05-24 17:43:43 +0000

    This commit was manufactured by cvs2svn to create branch 'ksh_maintainer'.
    Cherrypick from master 2004-04-27 20:00:23 UTC tg <tg@mirbsd.org> 'null commit for version bump to mirbsdksh 1.6':
        LEGAL
        config.h
        ksh.1tbl
        main.c
        proto.h
        tests/version.t
        var.c
        version.c
    Cherrypick from master 2004-05-24 17:43:42 UTC tg <tg@mirbsd.org> 'file strlfun.c was initially added on branch tg.':
        strlfun.c

---
+++ version.c
@@ -0,0 +1,11 @@
+/* $MirBSD: version.c,v 1.6 2004/04/27 20:00:22 tg Exp $ */
+/* $OpenBSD: version.c,v 1.12 1999/07/14 13:37:24 millert Exp $	*/
+
+/*
+ * value of $KSH_VERSION (or $SH_VERSION)
+ */
+
+#include "sh.h"
+
+const char ksh_version[] =
+	"@(#)PD KSH v5.2.14 99/07/13.2 MirOS $Revision: 1.6 $";
