d33837068fb5db5ba55444b98eefe49a61b5a9ba
root <root@mirbsd.org>
2004-08-27 20:05:43 +0000

    This commit was manufactured by cvs2svn to create tag 'mirbsdksh-1_12'.
    Sprout from master 2004-08-27 20:05:42 UTC tg <tg@mirbsd.org> '* I got tricked by <gecko2> to add install documentation!'
    Cherrypick from openbsd 2003-03-22 17:35:03 UTC tg <tg@mirbsd.org> 'Import OpenBSD 3.3 source repository from CTM 3132 the first time':
        tests/cdhist.t
        tests/unclass1.t
    Cherrypick from ksh_maintainer 2004-08-27 19:36:55 UTC tg <tg@mirbsd.org> 'copy latest src/lib/libc/string/strlfun.c':
        strlfun.c

---
+++ strlfun.c
@@ -0,0 +1,128 @@
+/**	$MirBSD: strlfun.c,v 1.1.4.1 2004/08/27 19:36:55 tg Exp $ */
+/*	$OpenBSD: strlcpy.c,v 1.8 2003/06/17 21:56:24 millert Exp $ */
+/*	$OpenBSD: strlcat.c,v 1.11 2003/06/17 21:56:24 millert Exp $ */
+
+/*-
+ * Copyright (c) 2004
+ *	Thorsten "mirabile" Glaser <tg@66h.42h.de>
+ *
+ * Licensee is hereby permitted to deal in this work without restric-
+ * tion, including unlimited rights to use, publically perform, modi-
+ * fy, merge, distribute, sell, give away or sublicence, provided the
+ * above copyright notices, these terms and the disclaimer are retai-
+ * ned in all redistributions, or reproduced in accompanying documen-
+ * tation or other materials provided with binary redistributions.
+ *
+ * Licensor hereby provides this work "AS IS" and WITHOUT WARRANTY of
+ * any kind, expressed or implied, to the maximum extent permitted by
+ * applicable law, but with the warranty of being written without ma-
+ * licious intent or gross negligence; in no event shall licensor, an
+ * author or contributor be held liable for any damage, direct, indi-
+ * rect or other, however caused, arising in any way out of the usage
+ * of covered work, even if advised of the possibility of such damage.
+ *-
+ * Implementation by myself
+ * Some optimizations idea from Bodo Eggert
+ */
+
+/*
+ * Copyright (c) 1998 Todd C. Miller <Todd.Miller@courtesan.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+
+#include <sys/types.h>
+#include <string.h>
+
+#ifdef	HAVE_CONFIG_H
+/* packaged with third-party software */
+#include "config.h"
+#define	LIBC_SCCS
+#else	/* ! def(HAVE_CONFIG_H) */
+/* integrated into C library */
+#undef	HAVE_STRLCPY
+#undef	HAVE_STRLCAT
+#endif	/* ! def(HAVE_CONFIG_H) */
+
+#if defined(__RCSID)
+__RCSID("$MirBSD: strlfun.c,v 1.1.4.1 2004/08/27 19:36:55 tg Exp $");
+#elif defined(LIBC_SCCS) && !defined(lint)
+static const char rcsid[] = "$MirBSD: strlfun.c,v 1.1.4.1 2004/08/27 19:36:55 tg Exp $";
+#endif	/* LIBC_SCCS and not lint */
+
+
+#ifndef	HAVE_STRLCPY
+/*
+ * Copy src to string dst of size siz.  At most siz-1 characters
+ * will be copied.  Always NUL terminates (unless siz == 0).
+ * Returns strlen(src); if retval >= siz, truncation occurred.
+ */
+size_t
+strlcpy(char *dst, const char *src, size_t siz)
+{
+	const char *s = src;
+
+	if (!siz) goto traverse_src;
+
+	/* Copy as many bytes as will fit */
+	for (; --siz && (*dst++ = *s++); /* nothing */)
+		;
+
+	/* Not enough room in dst, add NUL and traverse rest of src */
+	if (!siz) {
+		/* Save, since we've copied at max. (siz-1) characters */
+		*dst = '\0';	/* NUL-terminate dst */
+  traverse_src:
+		while (*s++)
+			;
+	}
+
+	return (s - src - 1);	/* count does not include NUL */
+}
+#endif	/* ndef HAVE_STRLCPY */
+
+#ifndef	HAVE_STRLCAT
+/*
+ * Appends src to string dst of size siz (unlike strncat, siz is the
+ * full size of dst, not space left).  At most siz-1 characters
+ * will be copied.  Always NUL terminates (unless siz <= strlen(dst)).
+ * Returns strlen(src) + MIN(siz, strlen(initial dst)).
+ * If retval >= siz, truncation occurred.
+ */
+size_t
+strlcat(char *dst, const char *src, size_t siz)
+{
+	char *d = dst;
+	size_t n = siz, dl;
+	const size_t sl = strlen(src);
+
+	while (n-- && (*d++ != '\0'))
+		;
+	if (!++n && (*d != '\0'))
+		return strlen(src);
+	dl = --d - dst;		/* original strlen(dst), max. siz-1 */
+	n = siz - dl;
+	dl += sl;
+
+	if (!n--)
+		return dl;
+
+	if (n > sl)
+		n = sl;		/* number of octets to copy */
+	for (; n-- && (*d++ = *src++); /* nothing */)
+		;
+	*d = '\0';		/* NUL-terminate dst */
+	return dl;
+}
+#endif	/* ndef HAVE_STRLCAT */
