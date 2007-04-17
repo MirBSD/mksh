002904664eca697453ea22f486f5bc01450c8732
tg <tg@mirbsd.org>
2007-04-17 23:51:32 +0000

    version bump here, too (CR-LF shebang)

--- sh.h
+++ sh.h
@@ -8,8 +8,8 @@
 /*	$OpenBSD: c_test.h,v 1.4 2004/12/20 11:34:26 otto Exp $	*/
 /*	$OpenBSD: tty.h,v 1.5 2004/12/20 11:34:26 otto Exp $	*/
 
-#define MKSH_SH_H_ID "$MirOS: src/bin/mksh/sh.h,v 1.123 2007/04/15 12:09:57 tg Exp $"
-#define MKSH_VERSION "R29 2007/04/15"
+#define MKSH_SH_H_ID "$MirOS: src/bin/mksh/sh.h,v 1.124 2007/04/17 23:51:32 tg Exp $"
+#define MKSH_VERSION "R29 2007/04/17"
 
 #if HAVE_SYS_PARAM_H
 #include <sys/param.h>
