c4826b28b8cce38de535f01a529754f8d4bf5a98
tg <tg@mirbsd.org>
2007-04-23 20:57:50 +0000

    possibly fix the last issue on AIX: NSIG is (63 +1)

--- Build.sh
+++ Build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $MirOS: src/bin/mksh/Build.sh,v 1.173 2007/04/23 20:37:15 tg Exp $
+# $MirOS: src/bin/mksh/Build.sh,v 1.174 2007/04/23 20:57:50 tg Exp $
 #-
 # Environment used: CC CFLAGS CPP CPPFLAGS LDFLAGS LIBS NOWARN NROFF TARGET_OS
 # CPPFLAGS recognised: MKSH_SMALL MKSH_ASSUME_UTF8 MKSH_NEED_MKNOD MKSH_NOPWNAM
@@ -683,7 +683,10 @@ if test 1 = $NEED_MKSH_SIGNAME; then
 	NSIG=`( echo '#include <signal.h>'; echo '#ifndef NSIG'; \
 	    echo '#define NSIG _NSIG'; echo '#endif'; echo mksh_cfg: NSIG ) | \
 	    vq "$CPP $CPPFLAGS" | grep mksh_cfg: | \
-	    sed 's/^mksh_cfg: \([0-9x]*\).*$/\1/'`
+	    sed 's/^mksh_cfg: \([0-9x ()+-]*\).*$/\1/'`
+	case $NSIG in
+	*[ ()+-]*) NSIG=`awk "BEGIN { print $NSIG }"` ;;
+	esac
 	NSIG=`printf %d "$NSIG" 2>/dev/null`
 	test $h = 1 && printf "NSIG=$NSIG ... "
 	test $NSIG -gt 1 || exit 1
