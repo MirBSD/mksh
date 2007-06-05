7e4a39b11c206b2732a8a8f75be999868ed8eeaf
tg <tg@mirbsd.org>
2007-06-05 20:57:46 +0000

    ignore any whitespace between preprocessor output tokens

--- Build.sh
+++ Build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $MirOS: src/bin/mksh/Build.sh,v 1.199 2007/06/05 20:18:54 tg Exp $
+# $MirOS: src/bin/mksh/Build.sh,v 1.200 2007/06/05 20:57:46 tg Exp $
 #-
 # Environment used: CC CFLAGS CPP CPPFLAGS LDFLAGS LIBS NOWARN NROFF TARGET_OS
 # CPPFLAGS recognised: MKSH_SMALL MKSH_ASSUME_UTF8 MKSH_NEED_MKNOD MKSH_NOPWNAM
@@ -750,7 +750,7 @@ test 1 = $NEED_MKSH_SIGNAME && if test $HAVE_CPP_DD = yes; then
 	NSIG=`( echo '#include <signal.h>'; echo '#ifndef NSIG'; \
 	    echo '#define NSIG _NSIG'; echo '#endif'; echo mksh_cfg: NSIG ) | \
 	    vq "$CPP $CPPFLAGS" | grep mksh_cfg: | \
-	    sed 's/^mksh_cfg: \([0-9x ()+-]*\).*$/\1/'`
+	    sed 's/^mksh_cfg:[	 ]*\([0-9x ()+-]*\).*$/\1/'`
 	case $NSIG in
 	*[\ +-]*) NSIG=`awk "BEGIN { print $NSIG }"` ;;
 	esac
@@ -763,7 +763,7 @@ test 1 = $NEED_MKSH_SIGNAME && if test $HAVE_CPP_DD = yes; then
 	    sort | while read name; do
 		( echo '#include <signal.h>'; echo mksh_cfg: SIG$name ) | \
 		    vq "$CPP $CPPFLAGS" | grep mksh_cfg: | \
-		    sed 's/^mksh_cfg: \([0-9x]*\).*$/\1:'$name/
+		    sed 's/^mksh_cfg:[	 ]*\([0-9x]*\).*$/\1:'$name/
 	done | grep -v '^:' | while IFS=: read nr name; do
 		nr=`printf %d "$nr" 2>/dev/null`
 		test $nr -gt 0 && test $nr -le $NSIG || continue
