5c766044027a675839f19349b9450a1ac1b64e78
tg <tg@mirbsd.org>
2009-04-05 13:07:06 +0000

    replaced cannot sort

--- Build.sh
+++ Build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-srcversion='$MirOS: src/bin/mksh/Build.sh,v 1.384 2009/04/05 12:35:28 tg Exp $'
+srcversion='$MirOS: src/bin/mksh/Build.sh,v 1.385 2009/04/05 13:07:06 tg Exp $'
 #-
 # Environment used: CC CFLAGS CPPFLAGS LDFLAGS LIBS NOWARN NROFF TARGET_OS
 # CPPFLAGS recognised:	MKSH_SMALL MKSH_ASSUME_UTF8 MKSH_NOPWNAM MKSH_NOVI
@@ -282,11 +282,11 @@ __setkey_r' >crypt.exp
 	: ${LIBS='-lcrypt'}
 	: ${HAVE_SETLOCALE_CTYPE=0}
 	;;
-BSD/OS)
-	: ${HAVE_SETLOCALE_CTYPE=0}
-	;;
 BeOS|Haiku)
 	warn=' and will currently not work'
+	;;
+BSD/OS)
+	: ${HAVE_SETLOCALE_CTYPE=0}
 	;;
 CYGWIN*)
 	: ${HAVE_SETLOCALE_CTYPE=0}
