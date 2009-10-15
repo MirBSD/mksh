8b4314cbcdb4d7f438f00c1bafed30067d49969b
tg <tg@mirbsd.org>
2009-10-15 16:25:15 +0000

    fix exec-function-environment-1 (I hope this doesn’t break anything else!)

--- exec.c
+++ exec.c
@@ -22,7 +22,7 @@
 
 #include "sh.h"
 
-__RCSID("$MirOS: src/bin/mksh/exec.c,v 1.66 2009/10/02 18:08:32 tg Exp $");
+__RCSID("$MirOS: src/bin/mksh/exec.c,v 1.67 2009/10/15 16:25:15 tg Exp $");
 
 static int comexec(struct op *, struct tbl *volatile, const char **,
     int volatile, volatile int *);
@@ -497,7 +497,7 @@ comexec(struct op *t, struct tbl *volatile tp, const char **ap,
 		if (keepasn_ok && tp && tp->type == CFUNC &&
 		    !(tp->flag & FKSH)) {
 			bourne_function_call = true;
-			type_flags = 0;
+			type_flags = EXPORT;
 		} else
 			type_flags = LOCAL|LOCAL_COPY|EXPORT;
 	}
