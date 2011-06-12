6807ce3100b3cbbbd64cc83c1c0799efadccea49
tg <tg@mirbsd.org>
2011-06-12 15:37:10 +0000

    drop long-deprecated 'set ±o arc4random'

--- misc.c
+++ misc.c
@@ -29,7 +29,7 @@
 #include <grp.h>
 #endif
 
-__RCSID("$MirOS: src/bin/mksh/misc.c,v 1.167 2011/06/12 14:45:34 tg Exp $");
+__RCSID("$MirOS: src/bin/mksh/misc.c,v 1.168 2011/06/12 15:37:08 tg Exp $");
 
 /* type bits for unsigned char */
 unsigned char chtypes[UCHAR_MAX + 1];
@@ -356,14 +356,6 @@ parse_args(const char **argv,
 				break;
 			}
 			i = option(go.optarg);
-#ifndef MKSH_NO_DEPRECATED_WARNING
-			if ((enum sh_flag)i == FARC4RANDOM) {
-				warningf(true, "Do not use set ±o arc4random,"
-				    " it will be removed in the next version"
-				    " of mksh!");
-				return (0);
-			}
-#endif
 			if ((i != (size_t)-1) && set == Flag(i))
 				/*
 				 * Don't check the context if the flag
