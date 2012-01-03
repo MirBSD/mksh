7ff96c3086738d0b299d69b30c0606ddb3780e02
tg <tg@mirbsd.org>
2012-01-03 16:03:22 +0000

    fix gccism; found during testing with tcc

--- Build.sh
+++ Build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-srcversion='$MirOS: src/bin/mksh/Build.sh,v 1.505 2012/01/03 01:40:13 tg Exp $'
+srcversion='$MirOS: src/bin/mksh/Build.sh,v 1.506 2012/01/03 16:03:22 tg Exp $'
 #-
 # Copyright (c) 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
 #		2011, 2012
@@ -1262,7 +1262,7 @@ else
 		#define EXTERN
 		#define MKSH_INCLUDES_ONLY
 		#include "sh.h"
-		__RCSID("$MirOS: src/bin/mksh/Build.sh,v 1.505 2012/01/03 01:40:13 tg Exp $");
+		__RCSID("$MirOS: src/bin/mksh/Build.sh,v 1.506 2012/01/03 16:03:22 tg Exp $");
 		int main(void) { printf("Hello, World!\n"); return (0); }
 EOF
 	case $cm in
@@ -1492,7 +1492,7 @@ ac_testdone
 ac_cppflags
 
 save_CFLAGS=$CFLAGS
-test 1 = $HAVE_CAN_WNOOVERFLOW && CFLAGS="$CFLAGS -Wno-overflow"
+test x1 = x$HAVE_CAN_WNOOVERFLOW && CFLAGS="$CFLAGS -Wno-overflow"
 ac_testn compile_time_asserts_$$ '' 'whether compile-time assertions pass' <<-'EOF'
 	#define MKSH_INCLUDES_ONLY
 	#include "sh.h"
