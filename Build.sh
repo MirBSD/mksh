0af5e1f288a9c0a6bb4adf21303df2e09a1162cb
tg <tg@mirbsd.org>
2012-04-06 23:10:50 +0000

    can’t forget LynxOS, although it’s still preliminary…

--- Build.sh
+++ Build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-srcversion='$MirOS: src/bin/mksh/Build.sh,v 1.541 2012/04/06 15:20:41 tg Exp $'
+srcversion='$MirOS: src/bin/mksh/Build.sh,v 1.542 2012/04/06 23:10:50 tg Exp $'
 #-
 # Copyright (c) 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
 #		2011, 2012
@@ -540,6 +540,9 @@ Linux)
 	esac
 	add_cppflags -DSETUID_CAN_FAIL_WITH_EAGAIN
 	: ${HAVE_REVOKE=0}
+	;;
+LynxOS)
+	oswarn="; it has minor issues"
 	;;
 MidnightBSD)
 	;;
@@ -1389,7 +1392,7 @@ else
 		#define EXTERN
 		#define MKSH_INCLUDES_ONLY
 		#include "sh.h"
-		__RCSID("$MirOS: src/bin/mksh/Build.sh,v 1.541 2012/04/06 15:20:41 tg Exp $");
+		__RCSID("$MirOS: src/bin/mksh/Build.sh,v 1.542 2012/04/06 23:10:50 tg Exp $");
 		int main(void) { printf("Hello, World!\n"); return (0); }
 EOF
 	case $cm in
