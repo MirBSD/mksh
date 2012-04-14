ea6b0c60f50297d79d86d06205853d4d5893878e
tg <tg@mirbsd.org>
2012-04-14 19:35:43 +0000

    write a bit about pcc, 386BSD, klibc

--- Build.sh
+++ Build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-srcversion='$MirOS: src/bin/mksh/Build.sh,v 1.549 2012/04/14 16:07:43 tg Exp $'
+srcversion='$MirOS: src/bin/mksh/Build.sh,v 1.550 2012/04/14 19:35:43 tg Exp $'
 #-
 # Copyright (c) 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
 #		2011, 2012
@@ -471,10 +471,6 @@ esac
 # Configuration depending on OS name
 case $TARGET_OS in
 386BSD)
-	# required fixes for 386BSD-0.0new are:
-	# http://groups.google.com/group/comp.unix.bsd/browse_thread/thread/1c6397039f10e76b?hl=en
-	# http://groups.google.com/group/comp.unix.bsd/browse_thread/thread/cb899f7ccb81550b?hl=en
-	# from 386BSD-0.1 on, it works out of the box
 	: ${HAVE_CAN_OTWO=0}
 	add_cppflags -DMKSH_NO_SIGSETJMP
 	add_cppflags -DMKSH_TYPEDEF_SIG_ATOMIC_T=int
@@ -1402,7 +1398,7 @@ else
 		#define EXTERN
 		#define MKSH_INCLUDES_ONLY
 		#include "sh.h"
-		__RCSID("$MirOS: src/bin/mksh/Build.sh,v 1.549 2012/04/14 16:07:43 tg Exp $");
+		__RCSID("$MirOS: src/bin/mksh/Build.sh,v 1.550 2012/04/14 19:35:43 tg Exp $");
 		int main(void) { printf("Hello, World!\n"); return (0); }
 EOF
 	case $cm in
