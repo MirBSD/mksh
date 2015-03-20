8641829e21ef0daaf618674043fbcc0f20ba3055
tg <tg@mirbsd.org>
2015-03-20 23:37:39 +0000

    fix comment no longer true since commit 1004DC49710562ACCB2

--- lex.c
+++ lex.c
@@ -23,7 +23,7 @@
 
 #include "sh.h"
 
-__RCSID("$MirOS: src/bin/mksh/lex.c,v 1.197 2015/03/14 05:23:15 tg Exp $");
+__RCSID("$MirOS: src/bin/mksh/lex.c,v 1.198 2015/03/20 23:37:39 tg Exp $");
 
 /*
  * states while lexing word
@@ -1080,8 +1080,7 @@ yylex(int cf)
 				 * pushed into an SREREAD) which is what
 				 * we want here anyway: find out whether
 				 * the alias name is followed by a POSIX
-				 * function definition (only the opening
-				 * parenthesis is checked though)
+				 * function definition
 				 */
 				++cp;
 			/* prefer functions over aliases */
