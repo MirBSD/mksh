f611f1dc963d91f9ea81ddec4deefed73eb89485
tg <tg@mirbsd.org>
2021-07-30 02:53:17 +0000

    unbreak -g build (-DDEBUG, as it assumes modern compilers anyway)

--- Build.sh
+++ Build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-srcversion='$MirOS: src/bin/mksh/Build.sh,v 1.780 2021/07/27 19:17:12 tg Exp $'
+srcversion='$MirOS: src/bin/mksh/Build.sh,v 1.781 2021/07/30 02:53:17 tg Exp $'
 #-
 # Copyright (c) 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
 #		2011, 2012, 2013, 2014, 2015, 2016, 2017, 2019,
@@ -595,7 +595,7 @@ do
 		;;
 	:-g)
 		# checker, debug, valgrind build
-		cpp_define DEBUG 1
+		add_cppflags -DDEBUG
 		CFLAGS="$CFLAGS -g3 -fno-builtin"
 		;;
 	:-j)
