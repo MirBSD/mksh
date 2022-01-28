53fdfd288674eac2c4fe430715f0f8a862a135a7
tg <tg@mirbsd.org>
2022-01-28 14:26:27 +0000

    FreeBSD make is too loud, it sets -v (in Build.sh -M mode)

--- Build.sh
+++ Build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-srcversion='$MirOS: src/bin/mksh/Build.sh,v 1.820 2022/01/28 07:01:08 tg Exp $'
+srcversion='$MirOS: src/bin/mksh/Build.sh,v 1.821 2022/01/28 14:26:27 tg Exp $'
+set +evx
 #-
 # Copyright (c) 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
 #		2011, 2012, 2013, 2014, 2015, 2016, 2017, 2019,
